bcc173d931c4c132dba41158b31c9411da752e8c
habib92i <7habibiz@gmail.com>
2021-05-03 23:29:25 -0400

    program refactored with functions

--- quiz/main.js
+++ quiz/main.js
@@ -2,18 +2,34 @@
 const quiz = [["What is Superman's real name?","Clark Kent"],
 ["What is Wonder Woman's real name?","Diana Prince"],
 ["What is Batman's real name?","Bruce Wayne"]];
+
+
+function start(quiz){
 //initialize the score variable that will keep track of the number of correct answer
 let score = 0;
 //Then we'll loop through the array using a for-of loop, assigning the variables question and answer to each key and value in the map.
 for(const [question,answer] of quiz){
-    const response = prompt(question);
+    const response = ask(question);
+    check(response, answer)
+}
+gameOver();
+
+// Function declarations
+function ask(question){
+    return prompt(question)
+}
+
+function check(response, answer){
     if(response === answer){
-        alert('Correct!');
+        alert('correct!');
         score++;
-    } else {
-        alert(`Wrong! The correct answer was ${answer}`);
+    }else{
+        alert(`Wrong Answer! The right answer was ${answer}`);
     }
 }
 
-// At the end of the game, report the player's score
-alert(`Game Over, you scored ${score} point${score !== 1 ? 's' : ''}`);
+function gameOver(){
+    alert(`Game Over, you scored ${score} point${score  > 1 ? 's' : ''}`)
+}
+}
+ start(quiz);
