4b849b2bb907cea85347ff6d1eb3d86232b08960
habib92i <7habibiz@gmail.com>
2021-05-23 18:42:56 -0400

    code refactored by using an object and namespacing

--- quiz/main.js
+++ quiz/main.js
@@ -1,35 +1,39 @@
 //create an array called quiz that contains all questions and answers
-const quiz = [["What is Superman's real name?","Clark Kent"],
-["What is Wonder Woman's real name?","Diana Prince"],
-["What is Batman's real name?","Bruce Wayne"]];
-
-
-function start(quiz){
-//initialize the score variable that will keep track of the number of correct answer
-let score = 0;
-//Then we'll loop through the array using a for-of loop, assigning the variables question and answer to each key and value in the map.
-for(const [question,answer] of quiz){
-    const response = ask(question);
-    check(response, answer)
-}
-gameOver();
-
-// Function declarations
-function ask(question){
-    return prompt(question)
-}
-
-function check(response, answer){
-    if(response === answer){
-        alert('correct!');
-        score++;
-    }else{
-        alert(`Wrong Answer! The right answer was ${answer}`);
+const quiz = [
+    {name:"Superman", realName: 'Clark Kent'},
+    {name: 'Wonder Woman', realName: 'Diana Prince'},
+    {name: 'Batman', realName: 'Bruce Wayne'},
+];
+const game = {
+    start(quiz){
+        this.questions = [...quiz];
+        
+        this.score = 0;
+        // main game loop
+        for(const question of this.questions){
+        this.question = question;
+        console.log(question)
+        this.ask();
+        }
+        // end of main game loop
+        this.gameOver();
+    },
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        const response =  prompt(question);
+        this.check(response);
+    },
+    check(response){
+        const answer = this.question.realName;
+        if(response === answer){
+        alert('Correct!');
+        this.score++;
+        } else {
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    },
+    gameOver(){
+        alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
     }
 }
-
-function gameOver(){
-    alert(`Game Over, you scored ${score} point${score  > 1 ? 's' : ''}`)
-}
-}
- start(quiz);
+game.start(quiz);
